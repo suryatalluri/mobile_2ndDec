98db547e8f5d3fe6d4ccde24270c860a2188f35e
Surya null <surya.talluri+1@wavemaker.com>
2015-12-02 07:31:14 +0000

    New files added - [style.css,glyphicons-halflings-regular.woff,theme.png,glyphicons-halflings-regular.svg,variables.less,variables.less,OpenSans-Regular.ttf,glyphicons-halflings-regular.ttf,style.less,glyphicons-halflings-regular.eot,glyphicons-halflings-regular.woff2,style.css,.wmproject.properties,glyphicons-halflings-regular.svg,glyphicons-halflings-regular.ttf,style.less,glyphicons-halflings-regular.woff2,glyphicons-halflings-regular.eot,glyphicons-halflings-regular.woff,Roboto-Regular.ttf,android] Files modified - [login.html,index.html,hrdb.properties,hrdb.log,.wmproject.properties,wmProperties.js] Refactored files - [] Fixed bugs - [] Removed files - [glyphicons-halflings-regular.woff,glyphicons-halflings-regular.woff,glyphicons-halflings-regular.ttf,style.less,glyphicons-halflings-regular.eot,Roboto-Regular.ttf,variables.less,style.css,glyphicons-halflings-regular.svg,style.less,glyphicons-halflings-regular.svg,glyphicons-halflings-regular.woff2,theme.png,style.css,.wmproject.properties,OpenSans-Regular.ttf,glyphicons-halflings-regular.eot,glyphicons-halflings-regular.woff2,glyphicons-halflings-regular.ttf,variables.less]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,1 +1,1 @@
-var _WM_APP_PROPERTIES = {"defaultLanguage":"en","type":"APPLICATION","homePage":"Main","platformType":"MOBILE","activeTheme":"mobile"}
+var _WM_APP_PROPERTIES = {"defaultLanguage":"en","type":"APPLICATION","homePage":"Main","platformType":"MOBILE","activeTheme":"lemon-orange"}
